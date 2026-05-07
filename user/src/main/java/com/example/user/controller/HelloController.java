fd1bca1fb46bd81e16c7362d1bc9536633be7b06
dee828 <dee828@foxmail.com>
2026-05-07 17:19:40 +0800

    feat(gateway): HelloController@hi 增加返回端口号 environment.getProperty("server.port")

--- user/src/main/java/com/example/user/controller/HelloController.java
+++ user/src/main/java/com/example/user/controller/HelloController.java
@@ -1,7 +1,9 @@
 package com.example.user.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +13,9 @@ public class HelloController {
     @Value("${custom.name}")
     String customName;
 
+    @Autowired
+    Environment environment;
+
     @GetMapping("/")
     public String hello(){
         return "hello";
@@ -18,6 +23,6 @@ public class HelloController {
 
     @GetMapping("hi")
     public String hi(){
-        return "hi " + customName;
+        return "hi " + customName + " port=" + environment.getProperty("server.port");
     }
 }
